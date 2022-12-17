562a4671e9d23d59a054a9df3d52d4bfe5248aa5
Maksim6767 <nikmaksim20@gmail.com>
2022-12-17 22:12:14 +0200

    done task

---
+++ src/components/theme.js
@@ -0,0 +1,26 @@
+export const theme = {
+    colors: {
+      text: '#228b22',
+      background: 'linear-gradient(45deg, #588bb0, #00d4b6, #fcf837)',
+      primary: '#454b1b',
+    },
+    space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
+    fontSizes: [12, 14, 16, 20, 24, 32, 48, 64],
+    fonts: {
+      body: 'Raleway, sans-serif',
+    },
+    fontWeight: {
+        body: 400,
+        heading: 700,
+        bold: 900,
+    },
+    borders: {
+        heading: '5px solid green',
+    },
+    radii: {
+        heading: '10px',
+    },
+    shadows: {
+        boxShadow: '4px 4px 33px 15px rgba(69, 75, 27, 0.8)',
+    },
+}
